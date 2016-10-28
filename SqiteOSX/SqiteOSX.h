ea7e77a333c77d9c5a53230acb594f78169c5ae6
tianbo <tb@bimt.com>
2016-10-28 17:23:49 +0800

    add osx

---
+++ SqiteOSX/SqiteOSX.h
@@ -0,0 +1,19 @@
+//
+//  SqiteOSX.h
+//  SqiteOSX
+//
+//  Created by bimt-bob on 2016/10/28.
+//  Copyright © 2016年 bimt-jlf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SqiteOSX.
+FOUNDATION_EXPORT double SqiteOSXVersionNumber;
+
+//! Project version string for SqiteOSX.
+FOUNDATION_EXPORT const unsigned char SqiteOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SqiteOSX/PublicHeader.h>
+
+
